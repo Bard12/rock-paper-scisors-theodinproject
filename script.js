4503d2f97e060b1082c85a83f739a54964f5de8f
Bardhi Ymeraga <bardhiymeraga@gmail.com>
2021-12-13 23:04:59 -0500

    Add script.js

--- script.js
+++ script.js
@@ -1,61 +1,55 @@
         //computerChoice is an array with 3 elements 
         const computerChoice=["Rock", "Paper", "Scissors"];
-        //computerPlay variable Contains  a function that chooses randomly an element from the computerChoice array
-        let computerPlay= computerChoice[Math.floor(Math.random()*computerChoice.length)];
-        console.log(computerPlay);
 
-        const playerChoice= prompt("Choose rock, paper, sccisors:", "");
-        let player= playerChoice.toLowerCase();
-        console.log(player);
         let playerScore= 0;
         let computerScore= 0;
 
         //rules is a function that contains the rules of the game and determines who wins the round
-        function rules(comp, guest) {
+        function rules() {
+
+            //computerPlay variable Contains  a function that chooses randomly an element from the computerChoice array
+            let computerPlay= computerChoice[Math.floor(Math.random()*computerChoice.length)];
+
+            const playerChoice= prompt("Choose rock, paper, sccisors:", "");
+            let player= playerChoice.toLowerCase();
+
             switch(true) {
-                case (computerPlay==("Rock") && playerChoice==("scissors")):
+                case (computerPlay==("Rock") && player==("scissors")):
                     alert("You Lose Rock beats Scissors");
+                    computerScore++;
                     break;
-                case (computerPlay==("Scissors") && playerChoice==("paper")):
+                case (computerPlay==("Scissors") && player==("paper")):
                     alert("You Lose Scissors beats Paper ");
+                    computerScore++;
                     break;
-                case (computerPlay==("Paper") && playerChoice==("rock")):
+                case (computerPlay==("Paper") && player==("rock")):
                     alert("You Lose Paper beats Rock");
+                    computerScore++;
                     break;
-                case (computerPlay==("Scissors") && playerChoice==("rock")):
+                case (computerPlay==("Scissors") && player==("rock")):
                     alert("You Win Rock beats Scissors");
+                    playerScore++;
                     break;
-                case (computerPlay==("Paper") && playerChoice==("scissors")):
-                    alert("You Win Scissors beats Paper");;
+                case (computerPlay==("Paper") && player==("scissors")):
+                    alert("You Win Scissors beats Paper");
+                    playerScore++;
                     break;
-                case (computerPlay==("Rock") && playerChoice==("paper")):
+                case (computerPlay==("Rock") && player==("paper")):
                     alert("You Win Paper beats Rock");
-;                    break;
-                case (computerPlay==("Rock") && playerChoice==("rock")):
-                    alert("It's Draw ");
+                    playerScore++;                   
                     break;
-                case (computerPlay==("Paper") && playerChoice==("paper")):
-                    alert("It's a Draw");
-                    break;
-                case (computerPlay==("Scissors") && playerChoice==("scissors")):
-                    alert("It's a Draw");
+                case (computerPlay.toLowerCase()===player):
+                    alert("Draw ");
                     break;
                 default:
                     alert("Invalid input");
             }
-        }    
-        rules(computerPlay, playerChoice);
-        alert(playerScore, computerScore)
-
-        //game is a function that includes the rules function and it makes the game play 5 rounds 
-        function game(rules) {
-            if (playerScore<3 || computerScore<3) {
-                return(rules);
-            }           
-            else if (computerScore>=3) {
-                return(alert("Try Again"));
-            }
-            else if (playerScore>=3) {
-                return(alert("You Won", "Player Score: "+playerScore, "Computer Score: "+computerScore))
-            }
         }
+        
+        for ((computerScore=0) || (playerScore=0); (computerScore<=5) || (playerScore<=5) ; (computerScore++) || (playerScore++)) {
+            rules();
+        }
+        console.log(computerScore, playerScore);
+        
+
+
