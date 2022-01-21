790321c083405cb6f8e439f19dcc1bb62baeac82
Bardhi Ymeraga <bardhiymeraga@gmail.com>
2022-01-20 20:10:03 -0500

    Add index.html

--- script.js
+++ script.js
@@ -6,16 +6,16 @@
         and in the computerPlay variable is a function that makes a random choice from the computerChoice array. */
         function rules() {
             let computerPlay= computerChoice[Math.floor(Math.random()*computerChoice.length)];
-            const playerChoice= prompt("Choose rock, paper, sccisors:", "");
+            //const playerChoice= prompt("Choose rock, paper, sccisors:", "");
             let player= playerChoice.toLowerCase();
             //switch statemnet  contains the rules of the game and determines who wins the round, also it adds 1 score to the winner.
             switch(true) {
                 case (computerPlay==("Rock") && player==("scissors")):
-                    alert("You Lose Rock beats Scissors");
+                    //alert("You Lose Rock beats Scissors");
                     computerScore++;
                     break;
                 case (computerPlay==("Scissors") && player==("paper")):
-                    alert("You Lose Scissors beats Paper ");
+                    //alert("You Lose Scissors beats Paper ");
                     computerScore++;
                     break;
                 case (computerPlay==("Paper") && player==("rock")):
@@ -23,19 +23,19 @@
                     computerScore++;
                     break;
                 case (computerPlay==("Scissors") && player==("rock")):
-                    alert("You Win Rock beats Scissors");
+                    //alert("You Win Rock beats Scissors");
                     playerScore++;
                     break;
                 case (computerPlay==("Paper") && player==("scissors")):
-                    alert("You Win Scissors beats Paper");
+                    //alert("You Win Scissors beats Paper");
                     playerScore++;
                     break;
                 case (computerPlay==("Rock") && player==("paper")):
-                    alert("You Win Paper beats Rock");
+                    //alert("You Win Paper beats Rock");
                     playerScore++;                   
                     break;
                 case (computerPlay.toLowerCase()===player):
-                    alert("Draw ");
+                    //alert("Draw ");
                     playerScore++;
                     computerScore++;
                     break;
@@ -44,7 +44,7 @@
             }
         }
         //for is a loop function that makes the game play until player or computer reaches 5 scores
-        let score =(computerScore || playerScore);
+        /*let score =(computerScore || playerScore);
         for (score=0; score<5; score++) {
             rules();
             alert("Player: "+playerScore);
@@ -55,7 +55,7 @@
         }
         else if (computerScore>playerScore){
             alert("Computer Won Try Again");
-        }
+        }*/
         
 
 
